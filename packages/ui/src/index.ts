3825cdd1d21c5e1510a65ce7877d88241399be04
Ewen LE QUERE <ewen.le-quere@epitech.eu>
2025-12-27 00:41:02 +0100

    feat(ui): add atomic design component library
    Atoms:
    - Button, Input, Label, Badge
    - Avatar, Skeleton, Separator
    
    Molecules:
    - Card, Tooltip
    
    Built with CVA, Radix UI and Tailwind CSS

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,37 @@
+// Utilities
+export { cn } from "./lib/utils";
+
+// Atoms
+export { Button, buttonVariants } from "./atoms/button";
+export type { ButtonProps } from "./atoms/button";
+
+export { Input } from "./atoms/input";
+export type { InputProps } from "./atoms/input";
+
+export { Label } from "./atoms/label";
+
+export { Badge, badgeVariants } from "./atoms/badge";
+export type { BadgeProps } from "./atoms/badge";
+
+export { Skeleton } from "./atoms/skeleton";
+
+export { Avatar, AvatarImage, AvatarFallback } from "./atoms/avatar";
+
+export { Separator } from "./atoms/separator";
+
+// Molecules
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./molecules/card";
+
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./molecules/tooltip";
